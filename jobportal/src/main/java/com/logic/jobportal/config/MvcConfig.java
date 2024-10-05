6782dc5f485618dfcf6c3ffc10457c1bf102760d
ahmedbasha10 <bashamido6060@gmail.com>
2024-10-05 16:40:43 +0300

    fix profile photo issue

--- jobportal/src/main/java/com/logic/jobportal/config/MvcConfig.java
+++ jobportal/src/main/java/com/logic/jobportal/config/MvcConfig.java
@@ -1,11 +1,13 @@
 package com.logic.jobportal.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+@Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
     private static final String UPLOAD_DIR = "photos";
